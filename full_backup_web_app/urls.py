1c7dc324874cb3d4ab5a4be91d850006e974470f
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-12-01 15:10:46 -0300

    adds start_backup view and template

--- full_backup_web_app/urls.py
+++ full_backup_web_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from full_backup_web_app.views import auth_login, home, signup, auth_logout, source_dir_func, dest_dir_func
+from full_backup_web_app.views import auth_login, home, signup, auth_logout, source_dir_func, dest_dir_func, start_backup
 
 urlpatterns = [
     path("", auth_login, name="login"),
@@ -8,5 +8,6 @@ urlpatterns = [
     path("home/", home, name="home"),
     path("home/source/", source_dir_func, name="source"),
     path("home/destination/", dest_dir_func, name="destination"),
+    path("home/start_backup", start_backup, name="start_backup"),
     path("logout/", auth_logout, name="logout"),
 ]
--- full_backup_web_app/views.py
+++ full_backup_web_app/views.py
@@ -1,3 +1,8 @@
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 # from .models import Customers, Products, Suppliers, Entry_notes
@@ -74,14 +79,180 @@ def select_os():
     os = platform.system()
     return os
 
+source_dir = ''
 def source_dir_func(response):
+    global source_dir
     source_dir = filedialog.askdirectory()
-    #source_dir = os.getcwd()
     return JsonResponse({'source': source_dir})
 
-
-
+dest_dir = ''
 def dest_dir_func(response):
+    global dest_dir
     dest_dir = filedialog.askdirectory()
-    #dest_dir = os.getcwd()
     return JsonResponse({'destination': dest_dir})
+
+class Backup:
+    def __init__(self, time_now=time.strftime('%H:%M:%S'), date_now=time.strftime('%d-%m-%y')):
+        self.__backup_destination = ''
+        self.__backup_source = ''
+        self.__time_now = time_now
+        self.__date_now = date_now
+
+    def get_time_now(self):
+        return self.__time_now
+
+    def get_date_now(self):
+        return self.__date_now
+
+    def open_move_file(self):
+        if os == "Windows":
+            open_file = "notepad " + file
+            subprocess.run(open_file)
+            cd = 'cd'
+            current_folder = subprocess.getoutput(cd)
+            log_to_folder = "move " + current_folder + '\\' + file + ' ' + self.__backup_destination
+            subprocess.run(log_to_folder, shell=True)
+            return file
+        elif os == "Linux":
+            open_file = "gedit " + file
+            subprocess.run(open_file, shell=True)
+            pwd = "pwd"
+            current_folder = subprocess.getoutput(pwd)
+            log_to_folder = "mv " + current_folder + "/" + file + " " + self.__backup_destination + "/" + file
+            subprocess.run(log_to_folder, shell=True)
+            return file
+
+    # Header of the Full Backup display
+    def header(self):
+        time_now = Backup.get_time_now(self)
+        header = '''
+      ===========================================================================
+    ||    ______ _    _ _      _       ____          _____ _  ___    _ _____     ||
+    ||   |  ____| |  | | |    | |     |  _ \   /\   / ____| |/ / |  | |  __ \    ||
+    ||   | |__  | |  | | |    | |     | |_)   /  \ | |    | ' /| |  | | |__) |   ||
+    ||   |  __| | |  | | |    | |     |  _ < / /\ \| |    |  < | |  | |  ___/    ||
+    ||   | |    | |__| | |____| |____ | |_) / ____ \ |____| . \| |__| | |        ||
+    ||   |_|     \____/|______|______||____/_/    \_\_____|_|\_ \____/|_|        ||
+    ||                                                                           ||
+    ||                    FULL BACKUP FROM THE FILE SERVER                       ||
+    ||                                                                           ||
+      ===========================================================================
+      ===========================================================================
+                     FULL BACKUP FROM THE FILE SERVER BEGAN AT {}
+      ===========================================================================
+                    >>> CLOSE THE FILE TO SEND IT VIA EMAIL! <<<
+        '''.format(time_now)
+        return header
+
+    def gen_backup(self):
+        date_now = Backup.get_date_now(self)
+        os = select_os()
+        backup_file_name = 'full_backup_{}.tar.gz'.format(date_now)
+        backup_source = source_dir
+        self.__backup_source = backup_source
+        backup_destination = dest_dir
+        self.__backup_destination = backup_destination
+        if os == "Linux":
+            backup = 'cd ' + str(backup_destination) + ' && tar -cf {} "{}" '.format(backup_file_name, backup_source)
+            subprocess.run(backup, shell=True)
+            return backup
+        elif os == "Windows":
+            backup = 'cd /d ' + str(backup_destination) + ' && tar -cf {} "{}" '.format(backup_file_name, backup_source)
+            subprocess.run(backup, shell=True)
+            return backup
+
+    def gen_list(self):
+        os = select_os()
+        if os == "Windows":
+            files = "cd /d " + self.__backup_source + " && dir /s"
+            files_out = subprocess.getoutput(files)
+            return files_out
+        elif os == "Linux":
+            files = "cd " + self.__backup_source + " && ls"
+            files_out = subprocess.getoutput(files)
+            return files_out
+
+    def generate_log(self):
+        date_now = Backup().get_date_now()
+        os = select_os()
+        file_log = 'full_backup_log_{}.txt'.format(date_now)
+        path_log = self.__backup_destination
+        if os == "Linux":
+            path_log = path_log + "/" + file_log
+            return path_log
+        elif os == "Windows":
+            path_log = path_log + "\\" + file_log
+            return path_log
+
+    def footer(self):
+        footer = '''
+        ===========================================================================
+                                     ENDED FULL BACKUP
+                ENDING DATE/TIME  : {}  -  {}
+                LOG FILE PATH     : {}
+                BKP FILE PATH     : {}
+        ===========================================================================
+        '''.format(time.strftime('%d/%m/%y'), time.strftime('%H:%M:%S'), self.__backup_destination,
+                   self.__backup_destination)
+        return footer
+
+    def subscribe_log(self, header, file_list, footer):
+        file = 'full_backup_log_{}.txt'.format(time.strftime('%d-%m-%y'))
+        f = open(file, 'w')
+        f.write(header)
+        f.write(file_list)
+        f.write(footer)
+        f.close()
+
+    def send_email(self, from_email, to_email, pass_email):
+        os = select_os()
+        global file
+        try:
+            fromaddr = from_email
+            toaddr = to_email
+            password = pass_email
+
+            msg = MIMEMultipart()
+
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = 'Full backup logs to Xterm'
+
+            body = '\n Please find attached the backup logs'
+
+            msg.attach(MIMEText(body, 'plain'))
+
+            file = 'full_backup_log_{}.txt'.format(time.strftime('%d-%m-%y'))
+            attachment = open(file, 'rb')
+
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload((attachment.read()))
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment; filename= {}'.format(file))
+
+            msg.attach(part)
+
+            attachment.close()
+
+            server = smtplib.SMTP('smtp.hostinger.com.br', 587)
+            server.starttls()
+            server.login(fromaddr, password)
+            text = msg.as_string()
+            server.sendmail(fromaddr, toaddr, text)
+            server.quit()
+
+            self.open_move_file()
+
+        except:
+
+            self.open_move_file()
+
+
+backup = Backup()
+
+def start_backup(request):
+    if request.method == "POST":
+        backup.gen_backup()
+        backup.generate_log()
+    return render(request, "auth/home.html")
+    
